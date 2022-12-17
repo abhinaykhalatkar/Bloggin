ff6dc3b209de2f2394f8449e85c8a9687bdd73c2
vinithuday <vinithrajbmu@gmail.com>
2022-12-17 22:24:38 +0100

    conflicts fixed

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -7,83 +7,9 @@
     </div>
     <!-- <BlogList> -->
   </div>
-
-  <!-- Blog Category -->
-  <div class="category-container">
-    <div class="outer-category">
-      <div class="category">All</div>
-    </div>
-    <div class="outer-category">
-      <div class="category">Sports</div>
-    </div>
-    <div class="outer-category">
-      <div class="category">Food </div>
-    </div>
-    <div class="outer-category">
-      <div class="category">Travel</div>
-    </div>
-    <div class="outer-category">
-      <div class="category">Lifestyle</div>
-    </div>
-    <div class="outer-category">
-      <div class="category">Personal</div>
-    </div>
+  <div class="blog-post">
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogPosts" :key="index" />
   </div>
-
-  <!-- Card Sliders -->
-  <div class="card-container">
-    <div class="card">
-      <img src="../assets/1.png" alt="Avatar">
-      <div class="card-info">
-        <h4><b>Title</b></h4>
-        <p>Description</p>
-      </div>
-    </div>
-    <div class="card">
-      <img src="../assets/1.png" alt="Avatar">
-      <div class="card-info">
-        <h4><b>Title</b></h4>
-        <p>Description</p>
-      </div>
-    </div>
-    <div class="card">
-      <img src="../assets/1.png" alt="Avatar">
-      <div class="card-info">
-        <h4><b>Title</b></h4>
-        <p>Description</p>
-      </div>
-    </div>
-  </div>
-
-  <!-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="@/assets/aboutUs/Mahatma-Gandhi.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="@/assets/aboutUs/pexels-picography-4458.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="@/assets/aboutUs/Mahatma-Gandhi.jpg" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div> -->
-
-
-
-
-
-
-
-  
 </template>
 
 <script>
@@ -150,52 +76,7 @@ export default {
   }
 }
 
-.outer-category {
-  box-shadow: 1px 1px 3px #888888;
-  border-radius: 5px;
-  width: 10%;
-  margin: 14px;
-  cursor: pointer;
+.blog-post {
+  margin-top: 5%;
 }
-
-.category {
-  text-align: center;
-  line-height: 40px;
-  font-size: 16px;
-  font-weight: bold;
-}
-
-.card-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  margin-top: 100px;
-}
-
-.card {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  // width: 40%;
-  border-radius: 5px;
-}
-
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-}
-
-img {
-  width: 50%;
-  align-content: center;
-  border-radius: 5px 5px 0 0;
-}
-
-.card-info {
-  padding: 2px 16px;
-}
-
 </style>
