267ee111f4ac4dd00457f952b76d1127f3da4ec8
vinithuday <vinithrajbmu@gmail.com>
2022-12-18 21:58:39 +0100

    template 4

--- front-end/src/components/TemplateStructure/Template4.vue
+++ front-end/src/components/TemplateStructure/Template4.vue
@@ -1,12 +1,219 @@
 <template>
-    <h1>template 4</h1>
-        
-    </template>
-    <script>
-    export default {
-      name: "template 4",
-      components:{}
-    }
+  <div class="Temp">
+    <h1>Travel</h1>
+
+  </div>
+  <div>
+    <img class="template4" :src="require('@/assets/Screen2Assets/Template.jpg')" alt="Placeholder image">
+
+    <div class="head"><br />
+      <h2><a class="text-dark" href="#">How I went from selling food in the street to working for top firms in tech
+        </a>
+      </h2>
+      <div class="blog">
+      <p class="card-text mb-auto">In this series of articles I want to share with you how I got into the world of
+      programming. I never went to university to study IT, but I found a way around it. If you like the series and
+      want to see a book out of this, please leave a comment below.
+
+      At the end of 2006, I arrived at a crossroads in my life. My hopes of becoming a secondary school
+      linguistics teacher had vanished in an instant, as several factors had come together and made it impossible
+      for me to continue with my studies.
+
+      Back in my hometown of Durazno, Uruguay, my wife was working long hours for a meager $160 (USD) a month.
+      Yes, that’s $1,920 a year. We had sacrificed our time together so I could become a teacher and get a better
+      job because we were dreaming of a better future.
+
+      The problem with dreams is they tend to vanish when you wake up, and life’s alarm clock had just gone off.
+
+      Because my career trajectory had suddenly strayed off course, I moved back to my hometown to figure out my
+      next steps. Needless to say, I was depressed at the way things were, and our living situation only made
+      things worse. It was good to be back with my wife, but the reasons for it were stressful.</p>
+    <div>― Alvaro Videla</div>
+    <div class="mb-1 text-muted dateposted">Posted On July 31,2022</div>
+    </div>
+  </div>
     
-    </script>
-    <style></style>
+
+
+
+
+
+        <div class="fr4-2">
+      <div class="card-body d-flex flex-column align-items-start">
+        <strong class="d-inline-block mb-2 text-primary">Food</strong>
+        <h3 class="mb-0">
+          <a class="text-dark" href="#">How I went from selling food in the street to working for top firms in tech
+          </a>
+        </h3>
+        <div class="mb-1 text-muted"><br />July 31,2022</div>
+        <p class="card-text mb-auto">In this series of articles I want to share with you how I got into the world of
+          programming. I never went to university to study IT, but I found a way around it. If you like the series and
+          want to see a book out of this, please leave a comment below.
+
+          At the end of 2006, I arrived at a crossroads in my life. My hopes of becoming a secondary school
+          linguistics teacher had vanished in an instant, as several factors had come together and made it impossible
+          for me to continue with my studies.
+
+          Back in my hometown of Durazno, Uruguay, my wife was working long hours for a meager $160 (USD) a month.
+          Yes, that’s $1,920 a year. We had sacrificed our time together so I could become a teacher and get a better
+          job because we were dreaming of a better future.
+
+          The problem with dreams is they tend to vanish when you wake up, and life’s alarm clock had just gone off.
+
+          Because my career trajectory had suddenly strayed off course, I moved back to my hometown to figure out my
+          next steps. Needless to say, I was depressed at the way things were, and our living situation only made
+          things worse. It was good to be back with my wife, but the reasons for it were stressful.</p>
+        <div>― Alvaro Videla</div>
+      </div>
+      <div class="card-body d-flex flex-column align-items-start">
+        <strong class="d-inline-block mb-2 text-primary">Food</strong>
+        <h3 class="mb-0">
+          <a class="text-dark" href="#">How I went from selling food in the street to working for top firms in tech
+          </a>
+        </h3>
+        <div class="mb-1 text-muted"><br />July 31,2022</div>
+        <p class="card-text mb-auto">In this series of articles I want to share with you how I got into the world of
+          programming. I never went to university to study IT, but I found a way around it. If you like the series and
+          want to see a book out of this, please leave a comment below.
+
+          At the end of 2006, I arrived at a crossroads in my life. My hopes of becoming a secondary school
+          linguistics teacher had vanished in an instant, as several factors had come together and made it impossible
+          for me to continue with my studies.
+
+          Back in my hometown of Durazno, Uruguay, my wife was working long hours for a meager $160 (USD) a month.
+          Yes, that’s $1,920 a year. We had sacrificed our time together so I could become a teacher and get a better
+          job because we were dreaming of a better future.
+
+          The problem with dreams is they tend to vanish when you wake up, and life’s alarm clock had just gone off.
+
+          Because my career trajectory had suddenly strayed off course, I moved back to my hometown to figure out my
+          next steps. Needless to say, I was depressed at the way things were, and our living situation only made
+          things worse. It was good to be back with my wife, but the reasons for it were stressful.</p>
+        <div>― Alvaro Videla</div>
+      </div>
+
+
+
+
+      <div class="card-body d-flex flex-column align-items-start">
+        <strong class="d-inline-block mb-2 text-primary">Food</strong>
+        <h3 class="mb-0">
+          <a class="text-dark" href="#">How I went from selling food in the street to working for top firms in tech
+          </a>
+        </h3>
+        <div class="mb-1 text-muted"><br />July 31,2022</div>
+        <p class="card-text mb-auto">In this series of articles I want to share with you how I got into the world of
+          programming. I never went to university to study IT, but I found a way around it. If you like the series and
+          want to see a book out of this, please leave a comment below.
+
+          At the end of 2006, I arrived at a crossroads in my life. My hopes of becoming a secondary school
+          linguistics teacher had vanished in an instant, as several factors had come together and made it impossible
+          for me to continue with my studies.
+
+          Back in my hometown of Durazno, Uruguay, my wife was working long hours for a meager $160 (USD) a month.
+          Yes, that’s $1,920 a year. We had sacrificed our time together so I could become a teacher and get a better
+          job because we were dreaming of a better future.
+
+          The problem with dreams is they tend to vanish when you wake up, and life’s alarm clock had just gone off.
+
+          Because my career trajectory had suddenly strayed off course, I moved back to my hometown to figure out my
+          next steps. Needless to say, I was depressed at the way things were, and our living situation only made
+          things worse. It was good to be back with my wife, but the reasons for it were stressful.</p>
+        <div>― Alvaro Videla</div>
+      </div>
+      <div class="card-body d-flex flex-column align-items-start">
+        <strong class="d-inline-block mb-2 text-primary">Food</strong>
+        <h3 class="mb-0">
+          <a class="text-dark" href="#">How I went from selling food in the street to working for top firms in tech
+          </a>
+        </h3>
+        <div class="mb-1 text-muted"><br />July 31,2022</div>
+        <p class="card-text mb-auto">In this series of articles I want to share with you how I got into the world of
+          programming. I never went to university to study IT, but I found a way around it. If you like the series and
+          want to see a book out of this, please leave a comment below.
+
+          At the end of 2006, I arrived at a crossroads in my life. My hopes of becoming a secondary school
+          linguistics teacher had vanished in an instant, as several factors had come together and made it impossible
+          for me to continue with my studies.
+
+          Back in my hometown of Durazno, Uruguay, my wife was working long hours for a meager $160 (USD) a month.
+          Yes, that’s $1,920 a year. We had sacrificed our time together so I could become a teacher and get a better
+          job because we were dreaming of a better future.
+
+          The problem with dreams is they tend to vanish when you wake up, and life’s alarm clock had just gone off.
+
+          Because my career trajectory had suddenly strayed off course, I moved back to my hometown to figure out my
+          next steps. Needless to say, I was depressed at the way things were, and our living situation only made
+          things worse. It was good to be back with my wife, but the reasons for it were stressful.</p>
+        <div>― Alvaro Videla</div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="template">
+    <div class="row-10">
+
+      <div class="col-md-11">
+
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+
+
+
+
+</template>
+<script>
+export default {
+  name: "template 4",
+  components: {}
+}
+
+</script>
+<style>
+.template4 {
+  width: 80%;
+  height: 30%;
+  margin: auto;
+  display: block;
+}
+
+.text-left {
+  text-align: left;
+  display: grid;
+}
+
+
+.Temp {
+  text-align: -webkit-center;
+}
+
+.fr4-2 {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10px;
+  padding: 0% 10% 0% 10%;
+  margin-bottom: 2%;
+}
+
+.head {
+  text-align: center;
+}
+
+.dateposted {
+  margin-bottom: 10px;
+  display: block;
+}
+.blog{
+  padding: 0% 10% 0% 10%;
+  margin-bottom: 3%;
+}
+
+</style>
