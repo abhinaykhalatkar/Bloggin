d9728e115b2d91a0137655b892545d5f7d74da00
virendrak07 <virenkadam07@gmail.com>
2022-12-18 21:01:43 +0100

    subscription design added

--- front-end/src/views/Subscription.vue
+++ front-end/src/views/Subscription.vue
@@ -1,91 +1,136 @@
 <template>
-    <div class="field">
-  <label class="label">Name</label>
-  <div class="control">
-    <input class="input" type="text" placeholder="Text input">
-  </div>
-</div>
-
-<div class="field">
-  <label class="label">Username</label>
-  <div class="control has-icons-left has-icons-right">
-    <input class="input is-success" type="text" placeholder="Text input" value="bulma">
-    <span class="icon is-small is-left">
-      <i class="fas fa-user"></i>
-    </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
-    </span>
-  </div>
-  <p class="help is-success">This username is available</p>
-</div>
-
-<div class="field">
-  <label class="label">Email</label>
-  <div class="control has-icons-left has-icons-right">
-    <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
-    <span class="icon is-small is-left">
-      <i class="fas fa-envelope"></i>
-    </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-exclamation-triangle"></i>
-    </span>
-  </div>
-  <p class="help is-danger">This email is invalid</p>
-</div>
-
-<div class="field">
-  <label class="label">Subject</label>
-  <div class="control">
-    <div class="select">
-      <select>
-        <option>Select dropdown</option>
-        <option>With options</option>
-      </select>
+  <div class="sub-container">
+    <div class="header-image">
+      <img class="header-img" src="../assets/sub.png" alt="">
+    </div>
+    <div class="login-container">
+      <div class="form-container">
+        <form>
+          <div class="header">
+            <h3> Welcome to Bloggen!</h3>
+          </div>
+          <div class="input-group">
+            <label for="">Plans</label>
+            <select>
+              <option>3 Month</option>
+              <option>6 Month</option>
+              <option>12 Month</option>
+            </select>
+          </div>
+          <div class="input-group">
+            <label for="psw"><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="psw" required>
+          </div>
+          <div class="input-group">
+            <label for="uname"><b>Contact</b></label>
+            <input type="text" placeholder="Enter Contact" name="uname" required>
+          </div>
+          <div class="input-group">
+            <label for="">Card Type</label>
+            <select>
+              <option>Visa</option>
+              <option>Master</option>
+              <option>Platinum</option>
+            </select>
+          </div>
+          <div class="input-group">
+            <label for="psw"><b>Card Number</b></label>
+            <input type="text" placeholder="Enter card number" name="psw" required>
+          </div>
+          <div class="input-group">
+            <label for="uname"><b>Card Holder Name</b></label>
+            <input type="text" placeholder="Enter name" name="uname" required>
+          </div>
+          <Button type="first" text="Submit"></Button>
+          <router-link :to="{ path: '/Registration' }"><Button type="second" text="Cancel" /></router-link>
+          <span class="psw"> <router-link :to="{ path: '/Email' }">
+            </router-link> </span>
+        </form>
+      </div>
+      <div class="login-image">
+        <img class="image" :src="require('@/assets/aboutUs/loginImage.jpg')" alt="Placeholder image">
+      </div>
     </div>
   </div>
-</div>
-
-<div class="field">
-  <label class="label">Message</label>
-  <div class="control">
-    <textarea class="textarea" placeholder="Textarea"></textarea>
-  </div>
-</div>
-
-<div class="field">
-  <div class="control">
-    <label class="checkbox">
-      <input type="checkbox">
-      I agree to the <a href="#">terms and conditions</a>
-    </label>
-  </div>
-</div>
-
-<div class="field">
-  <div class="control">
-    <label class="radio">
-      <input type="radio" name="question">
-      Yes
-    </label>
-    <label class="radio">
-      <input type="radio" name="question">
-      No
-    </label>
-  </div>
-</div>
-
-<div class="field is-grouped">
-  <div class="control">
-    <button class="button is-link">Submit</button>
-  </div>
-  <div class="control">
-    <button class="button is-link is-light">Cancel</button>
-  </div>
-</div>
 </template>
 <script>
-export default{
-  name:'subsciption'
-}</script>
-<style></style>
+import Button from '@/components/Button.vue';
+export default {
+  components: {
+    Button,
+  }
+};
+</script>
+<style lang="scss" scoped>
+.sub-container {
+  margin-top: 50px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: nowrap;
+  flex-direction: column;
+}
+
+.login-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 100px;
+  flex-wrap: nowrap;
+
+  .form-container {
+    border: 1px solid black;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex-wrap: nowrap;
+    align-items: center;
+    box-shadow: 3px 5px 5px #00000021;
+    border-radius: 15px;
+    padding: 70px 0px 70px 0px;
+    resize: vertical;
+  }
+
+  .input-group {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: stretch;
+    width: 100%;
+    flex-direction: column;
+
+    label {
+      color: #000000;
+      font-size: 17px;
+      font-weight: 500;
+    }
+
+    input[type="text"],
+    select {
+      font-size: 18px;
+      height: 50px;
+      width: 500px;
+      padding-left: 10px;
+      padding-right: 10px;
+      color: rgb(91, 91, 91);
+      border: 1px solid #383838;
+      border-radius: 4px;
+      background: white;
+      outline: none;
+    }
+  }
+
+
+
+  .login-image {
+    display: flex;
+    justify-content: center;
+  }
+
+  .image {
+    width: 95%;
+  }
+}
+</style>
